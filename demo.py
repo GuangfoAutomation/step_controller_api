1285fe890f3494397366cbc95a9246943b07ad64
GuangfoAutomation <a925493466@gmail.com>
2026-09-05 11:02:05 +0800

    调试

--- demo.py
+++ demo.py
@@ -12,129 +12,235 @@ dict_for_connect_custom = \
 controller = step_controller.ControllerSSHConnector(dict_for_connect_custom)
 
 def motor1_ccw_cw_360():
-    while(1):
-        controller.exec_command("ccw motor 1 degrees 360.0 speed 60.0")
-        time.sleep(1.3)
-        controller.exec_command("cw motor 1 degrees 360.0 speed 60.0")
-        time.sleep(1.3)
-
-def motor2_ccw_cw_360():
-    while(1):
-        controller.exec_command("ccw motor 2 degrees 360.0 speed 60.0")
-        time.sleep(1.3)
-        controller.exec_command("cw motor 2 degrees 360.0 speed 60.0")
-        time.sleep(1.3)
-
-def motor3_ccw_cw_360():
-    while(1):
-        controller.exec_command("ccw motor 3 degrees 360.0 speed 60.0")
-        time.sleep(1.3)
-        controller.exec_command("cw motor 3 degrees 360.0 speed 60.0")
-        time.sleep(1.3)
-
-def motor4_ccw_cw_360():
-    while(1):
-        controller.exec_command("ccw motor 4 degrees 360.0 speed 60.0")
-        time.sleep(1.3)
-        controller.exec_command("cw motor 4 degrees 360.0 speed 60.0")
-        time.sleep(1.3)
-
-def motor5_ccw_cw_360():
-    while(1):
-        controller.exec_command("ccw motor 5 degrees 360.0 speed 60.0")
-        time.sleep(1.3)
-        controller.exec_command("cw motor 5 degrees 360.0 speed 60.0")
-        time.sleep(1.3)
-
-def motor6_ccw_cw_360():
-    while(1):
-        controller.exec_command("ccw motor 6 degrees 360.0 speed 60.0")
-        time.sleep(1.3)
-        controller.exec_command("cw motor 6 degrees 360.0 speed 60.0")
-        time.sleep(1.3)
-
-def motor7_ccw_cw_360():
-    while(1):
-        controller.exec_command("ccw motor 7 degrees 360.0 speed 60.0")
-        time.sleep(1.3)
-        controller.exec_command("cw motor 7 degrees 360.0 speed 60.0")
-        time.sleep(1.3)
-
-def motor1_ccw_cw_to_intr1_2():
     status = 0
     while(1):
+        time.sleep(1.3)
         controller.exec_command("query status")
         log = controller.read_shell_output().strip()
         if "move_1: 0" in log: 
             print("电机1已空闲")
         else:
             print("电机1未空闲")
-            time.sleep(1)
+            continue
+
+        if status == 0:
+            controller.exec_command("ccw motor 1 degrees 360.0 speed 60.0")
+            status = 1
+            continue
+        else:
+            controller.exec_command("cw motor 1 degrees 360.0 speed 60.0")
+            status = 0
+            continue
+
+def motor2_ccw_cw_360():
+    status = 0
+    while(1):
+        time.sleep(1.3)
+        controller.exec_command("query status")
+        log = controller.read_shell_output().strip()
+        if "move_2: 0" in log: 
+            print("电机2已空闲")
+        else:
+            print("电机2未空闲")
+            continue
+
+        if status == 0:
+            controller.exec_command("ccw motor 2 degrees 360.0 speed 60.0")
+            status = 1
+            continue
+        else:
+            controller.exec_command("cw motor 2 degrees 360.0 speed 60.0")
+            status = 0
+            continue
+
+def motor3_ccw_cw_360():
+    status = 0
+    while(1):
+        time.sleep(1.3)
+        controller.exec_command("query status")
+        log = controller.read_shell_output().strip()
+        if "move_3: 0" in log: 
+            print("电机3已空闲")
+        else:
+            print("电机3未空闲")
+            continue
+
+        if status == 0:
+            controller.exec_command("ccw motor 3 degrees 360.0 speed 60.0")
+            status = 1
+            continue
+        else:
+            controller.exec_command("cw motor 3 degrees 360.0 speed 60.0")
+            status = 0
+            continue
+
+def motor4_ccw_cw_360():
+    status = 0
+    while(1):
+        time.sleep(1.3)
+        controller.exec_command("query status")
+        log = controller.read_shell_output().strip()
+        if "move_4: 0" in log: 
+            print("电机4已空闲")
+        else:
+            print("电机4未空闲")
+            continue
+
+        if status == 0:
+            controller.exec_command("ccw motor 4 degrees 360.0 speed 60.0")
+            status = 1
+            continue
+        else:
+            controller.exec_command("cw motor 4 degrees 360.0 speed 60.0")
+            status = 0
+            continue
+
+def motor5_ccw_cw_360():
+    status = 0
+    while(1):
+        time.sleep(1.3)
+        controller.exec_command("query status")
+        log = controller.read_shell_output().strip()
+        if "move_5: 0" in log: 
+            print("电机5已空闲")
+        else:
+            print("电机5未空闲")
+            continue
+
+        if status == 0:
+            controller.exec_command("ccw motor 5 degrees 360.0 speed 60.0")
+            status = 1
+            continue
+        else:
+            controller.exec_command("cw motor 5 degrees 360.0 speed 60.0")
+            status = 0
+            continue
+
+def motor6_ccw_cw_360():
+    status = 0
+    while(1):
+        time.sleep(1.3)
+        controller.exec_command("query status")
+        log = controller.read_shell_output().strip()
+        if "move_6: 0" in log: 
+            print("电机6已空闲")
+        else:
+            print("电机6未空闲")
+            continue
+
+        if status == 0:
+            controller.exec_command("ccw motor 6 degrees 360.0 speed 60.0")
+            status = 1
+            continue
+        else:
+            controller.exec_command("cw motor 6 degrees 360.0 speed 60.0")
+            status = 0
+            continue
+
+def motor7_ccw_cw_360():
+    status = 0
+    while(1):
+        time.sleep(1.3)
+        controller.exec_command("query status")
+        log = controller.read_shell_output().strip()
+        if "move_7: 0" in log: 
+            print("电机7已空闲")
+        else:
+            print("电机7未空闲")
+            continue
+
+        if status == 0:
+            controller.exec_command("ccw motor 7 degrees 360.0 speed 60.0")
+            status = 1
+            continue
+        else:
+            controller.exec_command("cw motor 7 degrees 360.0 speed 60.0")
+            status = 0
+            continue
+
+def motor1_ccw_cw_to_intr1_2():
+    status = 0
+    while(1):
+        time.sleep(1)
+        controller.exec_command("query status")
+        log = controller.read_shell_output().strip()
+        if "move_1: 0" in log: 
+            print("电机1已空闲")
+        else:
+            print("电机1未空闲")
             continue
 
         if status == 0:
             controller.exec_command("ccw motor 1 to intr 1 speed 60.0")
             status = 1
+            continue
         else:
             controller.exec_command("cw motor 1 to intr 2 speed 60.0")
             status = 0
+            continue
 
 def motor2_ccw_cw_to_intr7_8():
     status = 0
     while(1):
+        time.sleep(1)
         controller.exec_command("query status")
         log = controller.read_shell_output().strip()
         if "move_2: 0" in log: 
             print("电机2已空闲")
         else:
             print("电机2未空闲")
-            time.sleep(1)
             continue
 
         if status == 0:
             controller.exec_command("ccw motor 2 to intr 7 speed 60.0")
             status = 1
+            continue
         else:
             controller.exec_command("cw motor 2 to intr 8 speed 60.0")
             status = 0
+            continue
 
 def motor1_ccw_cw_freq_time():
     status = 0
     while(1):
+        time.sleep(1)
         controller.exec_command("query status")
         log = controller.read_shell_output().strip()
         if "move_1: 0" in log: 
             print("电机1已空闲")
         else:
             print("电机1未空闲")
-            time.sleep(1)
             continue
 
         if status == 0:
             controller.exec_command("ccw motor 1 freq 3200.0 time 3.0")
             status = 1
+            continue
         else:
             controller.exec_command("cw motor 1 freq 3200.0 time 3.0")
             status = 0
+            continue
 
 def motor2_ccw_cw_freq_time():
     status = 0
     while(1):
+        time.sleep(1)
         controller.exec_command("query status")
         log = controller.read_shell_output().strip()
         if "move_2: 0" in log: 
             print("电机2已空闲")
         else:
             print("电机2未空闲")
-            time.sleep(1)
             continue
 
         if status == 0:
             controller.exec_command("ccw motor 2 freq 3200.0 time 3.0")
             status = 1
+            continue
         else:
             controller.exec_command("cw motor 2 freq 3200.0 time 3.0")
             status = 0
+            continue
 
 if __name__ == "__main__":
     controller.connect()
